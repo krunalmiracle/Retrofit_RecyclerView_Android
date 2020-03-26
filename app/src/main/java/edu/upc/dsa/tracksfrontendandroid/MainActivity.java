c17fa0a7ca37cbfafa745927261815f3b4116c86
krunal badsiwal <krunalmiracle@gmail.com>
2020-03-26 12:32:15 +0100

    Implemented Retrofit Testing Phase

--- app/src/main/java/edu/upc/dsa/tracksfrontendandroid/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/tracksfrontendandroid/MainActivity.java
@@ -20,63 +20,61 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Retrofit
+
+        //Implementing RecyclerView
+
+
+    }
+    public void onButtonClick(View view) {
+        //Retrofit Implementation on Button Press
         Button button1 = findViewById(R.id.button);
         final TextView textView = findViewById(R.id.textView);
-        button1.setOnClickListener(new View.OnClickListener()
-        {
+        Toast toast= Toast.makeText(MainActivity.this,"Button Pressed!",Toast.LENGTH_SHORT);
+        toast.show();
+        textView.setText("Button Pressed!");
+        //Retrofit Implementation
+        //Adding Interceptor
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        //Attaching Interceptor to a client
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(interceptor).build();
+
+        // Running Retrofit to get result from Github service Interface
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
+        Call<List<Repo>> repos = service.listRepos("krunalmiracle");
+        /* Android Doesn't allow synchronous execution of Http Request and so we have to change this*/
+        repos.enqueue(new Callback<List<Repo>>() {
             @Override
-            public void onClick(View v) {
-
-                Toast toast= Toast.makeText(MainActivity.this,"Button Pressed!",Toast.LENGTH_SHORT);
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                //textView.setText("Completed!");
+                Toast toast= Toast.makeText(MainActivity.this,"Response Ok",Toast.LENGTH_SHORT);
                 toast.show();
-                textView.setText("Button Pressed!");
-                //Retrofit Implementation
-                //Adding Interceptor
-                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-                interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-                //Attaching Interceptor to a client
-                OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(interceptor).build();
+                String res_str = "";
+                List<Repo> result = response.body();
+                assert result != null;
+                for (Repo r:result) {
+                    res_str = r.toString() + "\n" + res_str;
+                }
+                textView.setText(res_str);
+            }
 
-                // Running Retrofit to get result from Github service Interface
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://api.github.com/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .client(client)
-                        .build();
-
-                GitHubService service = retrofit.create(GitHubService.class);
-                Call<List<Repo>> repos = service.listRepos("krunalmiracle");
-                /* Android Doesn't allow synchronous execution of Http Request and so we have to change this*/
-                repos.enqueue(new Callback<List<Repo>>() {
-                    @Override
-                    public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
-                        //textView.setText("Completed!");
-                        Toast toast= Toast.makeText(MainActivity.this,"Response Ok",Toast.LENGTH_SHORT);
-                        toast.show();
-                        String res_str = "";
-                        List<Repo> result = response.body();
-                        assert result != null;
-                        for (Repo r:result) {
-                            res_str = r.toString() + "\n" + res_str;
-                        }
-                        textView.setText(res_str);
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<Repo>> call, Throwable t) {
-                        Toast toast= Toast.makeText(MainActivity.this,"Error",Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                });
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                Toast toast= Toast.makeText(MainActivity.this,"Error",Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
-
-
 
     }
 }
