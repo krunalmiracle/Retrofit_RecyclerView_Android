123eb7b12a4ccef945e0aa63ce198e8f4b9ed977
krunal badsiwal <krunalmiracle@gmail.com>
2020-03-26 12:53:47 +0100

    Implemented RecyclerView & RetroFit Testing Phase both of them implemented both of them together.

--- app/src/main/java/edu/upc/dsa/tracksfrontendandroid/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/tracksfrontendandroid/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.OkHttpClient;
@@ -18,15 +19,35 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Implementing RecyclerView
+        recyclerView = findViewById(R.id.my_recycler_view);
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(false);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new MyAdapter(input);
+        recyclerView.setAdapter(mAdapter);
 
 
     }
---
+++ app/src/main/java/edu/upc/dsa/tracksfrontendandroid/MyAdapter.java
@@ -0,0 +1,83 @@
+package edu.upc.dsa.tracksfrontendandroid;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>  {
+
+    private List<String> values;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView txtHeader;
+        public TextView txtFooter;
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            txtHeader = v.findViewById(R.id.firstLine);
+            txtFooter = v.findViewById(R.id.secondLine);
+        }
+    }
+
+    public void add(int position, String item) {
+        values.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        values.remove(position);
+        notifyItemRemoved(position);
+    }
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(List<String> myDataset) {
+        values = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // create a new view
+        LayoutInflater inflater = LayoutInflater.from(
+                parent.getContext());
+        View v =
+                inflater.inflate(R.layout.row_layout, parent, false);
+        // set the view's size, margins, padding and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        final String name = values.get(position);
+        final int pos = holder.getLayoutPosition();
+        holder.txtHeader.setText(name);
+        holder.txtHeader.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(values.indexOf(name));
+            }
+        });
+
+        holder.txtFooter.setText(new StringBuilder().append("Footer: ").append(name).toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+}
